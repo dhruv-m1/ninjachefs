f02a31b7f3bda2912592aa412fd06c9e143395b9
Dhruv Malik <26849655+dhruv-tech@users.noreply.github.com>
2023-12-01 12:48:21 -0800

    disable signups

--- frontend/src/pages/auth/AuthSignUp.jsx
+++ frontend/src/pages/auth/AuthSignUp.jsx
@@ -9,7 +9,7 @@ export default function AuthSignUp() {
             <div className="bg-ninja-blue min-h-[287px] h-[35vh] w-full absolute right-0 z-0" aria-hidden />
 
             <section className="flex flex-col justify-center items-center min-h-[651px] h-[85vh]">
-                <SignUp
+                {/*<SignUp
                 
                     appearance={{
                         elements: {
@@ -19,13 +19,20 @@ export default function AuthSignUp() {
                         }
                     }}
 
-                />
+                />*/}
+
+                <div className="rounded-xl bg-white shadow-ninja text-poppins px-10 py-20 z-10">
+                    <h1 className="font-semibold text-lg">
+                    <i class="fa-solid fa-user-lock"></i>&thinsp;
+                        Invitation Required
+                    </h1>
+                    <p className="text-gray-500">Sorry, but sign ups are currently disabled.</p>
+                </div>
 
                 <div className="mt-6">
                     <p className="text-xs text-center text-gray-500">
                         
-                        Username-based signups are no longer unavailable. <br/>
-                        However, you can continue to use your existing username-based account.
+                        Thanks for your understanding.<br/>
 
                     </p>
                 </div>
